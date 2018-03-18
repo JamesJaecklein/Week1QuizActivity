7347be3d44e6ee63eda29f521f130392f64575eb
james <jamesjaecklein@gmail.com>
2018-03-18 12:20:32 -0400

    First Commit

---
+++ app/src/main/java/com/example/james/quizactivity/Question.java
@@ -0,0 +1,32 @@
+package com.example.james.quizactivity;
+
+/**
+ * Created by james on 3/15/2018.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
